68621c32a68294dc08a22d2c0486ddffb8a69bcb
Kristina Bulatović <bulatovic.tina95@gmail.com>
2020-05-10 15:08:36 +0200

    Added default category type

--- src/app/models/category.ts
+++ src/app/models/category.ts
@@ -3,7 +3,8 @@ export enum Category {
     Vegetables = 1,
     Meat = 2,
     Sweets = 3,
-    Drink = 4
+    Drink = 4,
+    Other = 5
 }
 
 export const CategoryMapping = {
@@ -11,5 +12,6 @@ export const CategoryMapping = {
     [Category.Vegetables]: 'Vegetables',
     [Category.Meat]: 'Meat',
     [Category.Sweets]: 'Sweets',
-    [Category.Drink]: 'Drink'
+    [Category.Drink]: 'Drink',
+    [Category.Other]: 'Other'
 };
--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -4,7 +4,7 @@ export class Product {
     public id: number;
     public name: string;
     public description: string;
-    public category: Category;
+    public category: Category = Category.Other;
     public manufacturer: string;
     public supplier: string;
     public price: number;
